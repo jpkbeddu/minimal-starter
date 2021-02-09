03c6a514da7ff20b49fc7221c3a9de6c075e7ae3
Prasanna Kumar <prasanna@tmmt.com>
2021-02-09 13:28:04 +0530

    Adds more API test routes

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('*', (req, res) => {
-  console.log('Req Headers', req.headers);
-  res.send('OK')
-})
+app.get('/', (req, res) => {
+  console.log('Default Req Headers', req.headers);
+  res.send('Default');
+});
+
+app.get('/third-party', (req, res) => {
+  console.log('third-party > Req Headers', req.headers);
+  res.send('third-party');
+});
+
+app.get('/google-maps', (req, res) => {
+  console.log('google-maps > Req Headers', req.headers);
+  res.send('google-maps');
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('App listening on port ' + listener.address().port)
-})
+  console.log('App listening on port ' + listener.address().port);
+});
